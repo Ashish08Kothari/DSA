3c941f5626ea47ceb8ac2fbee16bb0ba6029d3ab
Ashish Kotahri <ashish258kothari@gmail.com>
2026-05-24 23:14:05 +0530

    Day 1 done

---
+++ Reverse_A_Number.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Solution{
+    public static void main(String... args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        Solution obj = new Solution();
+        System.out.println(obj.reverse(n));
+    }
+
+    public int reverse(int x){
+        int sign = x >= 0 ? 1 : -1;
+        x = Math.abs(x);
+        int max = Integer.MAX_VALUE / 10, min = Integer.MIN_VALUE / 10;
+        int ans = 0;
+        while(x != 0){
+            if(ans > max || ans < min) return 0;
+            ans *= 10;
+            ans += x % 10;
+            x /= 10;
+        }
+
+        return ans * sign;
+    }
+}
+
+//Time Complexity: O(log10 n)
+//Space Complexity: O(1)
+

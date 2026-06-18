f0942852ef441dfcac3be2081a2e9fef241b432e
Ashish Kotahri <ashish258kothari@gmail.com>
2026-06-18 11:21:03 +0530

    Day13

---
+++ Binary_Search.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static void main(String[] args) {
+        int[] arr = {-1,0,3,5,9,12};
+        System.out.println(search(arr, 10));
+    }
+
+    public static int search(int[] arr, int target) {
+        int s = 0, e = arr.length-1;
+        while (s <= e) {
+            int mid = s+(e-s)/2;
+            int num = arr[mid];
+            if (num == target)
+                return mid;
+            else if (num < target)
+                s++;
+            else 
+                e--;
+        }
+
+        return -1;
+    }
+}
+
+/*
+    Time Complexity: O(log n)
+    Space Complexity: O(1)
+*/

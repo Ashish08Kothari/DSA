77db0670da6878250194b6541523a2939cf96637
Ashish Kotahri <ashish258kothari@gmail.com>
2026-06-18 12:05:44 +0530

    Day13

---
+++ Ceil_in_a_Sorted_Array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static void main(String[] arg) {
+        int[] arr = {1, 2, 8, 10, 11, 12, 19};
+        System.out.println(new Solution().findCeil(arr,5));
+    }
+
+    public int findCeil(int[] arr, int x) {
+        int n = arr.length, ans = -1;
+        int s = 0, e = n-1;
+        while (s <= e) {
+            int m = s + (e-s) / 2;
+            if (arr[m] >= x) {
+                ans = m;
+                e--;
+            }
+
+            else
+                s++;
+        }
+
+        return ans;
+    }
+}
+
+/*
+    Time Complexity : O(log n)
+    Space Complexity : O(1)
+ */

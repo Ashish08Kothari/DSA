29c39a860342d7db2596310e608bb0fd4ea3f1a1
Ashish Kotahri <ashish258kothari@gmail.com>
2026-06-18 11:28:57 +0530

    Day13

---
+++ Implement_Lower_Bound.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static void main(String... args) {
+        int[] arr = {2, 3, 7, 10, 11, 11, 25};
+        System.out.print(new Solution().lowerBound(arr,100));
+    }
+
+    int lowerBound(int[] arr, int target) {
+        int n = arr.length;
+        int s = 0, e = n-1;
+        while (s <= e) {
+            int m = s + (e-s) / 2;
+            if(arr[m] < target)
+                s++;
+            else
+                e--;
+        }
+
+        return s;
+    }
+}
+
+
+/*
+    Time Complexity : O(log n)
+    Space Complexity: O(1)
+*/

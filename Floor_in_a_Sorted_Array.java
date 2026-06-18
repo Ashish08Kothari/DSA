c2868b0200a7b703c4fd1ca839202d87da542a9b
Ashish Kotahri <ashish258kothari@gmail.com>
2026-06-18 12:00:15 +0530

    Day13

---
+++ Floor_in_a_Sorted_Array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findFloor(int[] arr, int x) {
+        int n = arr.length, ans = -1;
+        int s = 0, e = n-1;
+        while (s <= e) {
+            int m = s + (e-s) / 2;
+            if(arr[m] <= x) {
+                ans = m;
+                s++;
+            }
+            else {
+                e--;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 8, 10, 10, 12, 19};
+        System.out.println(new Solution().findFloor(arr,0));
+    }
+}
+
+/* Time Complexity : O(log n)
+   Space Complexity : O(1)
+*/

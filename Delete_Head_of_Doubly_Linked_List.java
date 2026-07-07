558d4b88742630ce997a4fee6a021ddec2dc678b
Ashish Kotahri <ashish258kothari@gmail.com>
2026-07-07 09:32:34 +0530

    Day28

---
+++ Delete_Head_of_Doubly_Linked_List.java
@@ -0,0 +1,30 @@
+/*
+class Node
+{
+    int data;
+    Node next, prev;
+    Node(int data)
+    {
+        this.data = data;
+        this.next = this.prev = null;
+    }
+}
+*/
+
+// Complete the function
+class Solution {
+    public static Node deleteHead(Node head) {
+        // your code here
+        Node temp = head;
+        head = head.next;
+        head.prev = null;
+        temp.next = null;
+        
+        return head;
+    }
+}
+
+/*
+    Time Complexity : O(1)
+    Space Complexity : O(1)
+*/
